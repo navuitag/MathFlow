fc61120b060d6f5565a80bd627472cda23c63805
Vu Nguyen <navuitag@gmail.com>
2026-06-12 20:18:49 +0700

    Add study time tracking with daily and total hours on home, profile, and learner switcher.

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "mathflow-vn-v19";
+const CACHE_NAME = "mathflow-vn-v20";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -14,6 +14,7 @@ const APP_SHELL = [
   "./assets/js/router.js",
   "./assets/js/state.js",
   "./assets/js/profileStore.js",
+  "./assets/js/studyTime.js",
   "./assets/js/utils.js",
   "./assets/js/mathFormat.js",
   "./assets/js/specialTopicMathFix.js",
